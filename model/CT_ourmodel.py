f40f300c5354637a44e2f595d2235604192840fb
kmlee307 <kyung45250@postech.ac.kr>
2024-03-08 11:55:47 +0000

    [Modify] add parameters and positional encoding

--- model/CT_ourmodel.py
+++ model/CT_ourmodel.py
@@ -1,7 +1,8 @@
 from pytorch_lightning import LightningModule
 import torch
 from torch import nn
-from utils import TransformerMultiInputblock, BROutcomeHead
+from utils_transformer import TransformerMultiInputBlock, AbsolutePositionalEncoding, RelativePositionalEncoding
+from utils import BROutcomeHead
 '''
 수진
 - active entries?? 무슨 용도인지 아직 모르겠음. 
@@ -12,10 +13,39 @@ class CT(LightningModule):
     def __init__(self, ):
         super().__init__()
 
-        self.basic_block_cls = TransformerMultiInputblock
+        self.basic_block_cls = TransformerMultiInputBlock
+
+        # Params for input transformation
+        self.dim_A = None # TBD or get from init input
+        self.dim_X = None # TBD or get from init input
+        self.dim_Y = None # TBD or get from init input
+        self.dim_V = None # TBD or get from init input
+        self.dim_hidden = None # TBD
+
+        # Parameters for basic block cls
+        self.seq_hidden_units = None #TBD
+        self.num_heads = None #TBD
+        self.head_size = None #TBD
+        self.dropout_rate = None #TBD
+        self.num_layer = None #TBD
+
+        # Params for poisitional encoding
+        # Follow the parameter of ct config file https://github.com/Valentyn1997/CausalTransformer/blob/27b253affa1a1e5190452be487fcbd45093dca00/config/backbone/ct.yaml#L20
+        # Type of positional_encoding (abolute/relative) - Appendix D
+        self.positional_encoding_absolute = False # use relative cause it is better
+        self.max_seq_length = 60 #Originally it's different for each data (cancer sim:60, mimic3_real:60, mimic3_synthetic:100)
+        self.positional_encoding_trainable = True
+        self.pe_max_relative_position = 15
+
         sub_args = None
         self.__init__specific(sub_args)
-    
+
+        
+    """
+    (경민)
+    우리 한가지 case(multi)인데 init이랑 init specific이 나누어야 할까?
+    sub args나 외부 argument쓰는게 헷갈리게 만드는 요인같은데 일단 가능한 거는 init함수에 써볼게!
+    """
     def __init__specific(self, sub_args):
         """
         Initialization of specific sub-network (only multi)
@@ -35,17 +65,39 @@ class CT(LightningModule):
         '''
         We need to take these weights to interpret attention matrix
         '''
-        self.A_input_transformation = nn.Linear(dim_A, dim_hidden) 
-        self.X_input_transformation = nn.Linear(dim_X, dim_hidden)
-        self.Y_input_transformation = nn.Linear(dim_Y, dim_hidden) 
-        self.V_input_transformation = nn.Linear(dim_V, dim_hidden)
+        self.A_input_transformation = nn.Linear(self.dim_A, self.dim_hidden) 
+        self.X_input_transformation = nn.Linear(self.dim_X, self.dim_hidden)
+        self.Y_input_transformation = nn.Linear(self.dim_Y, self.dim_hidden) 
+        self.V_input_transformation = nn.Linear(self.dim_V, self.dim_hidden)
         self.n_inputs = 3
 
-        # transformer blocks 
-        self.transformer_blocks = nn.ModuleList(
-            [self.basic_block_cls()]
+        # Init of positional encoding https://github.com/Valentyn1997/CausalTransformer/blob/27b253affa1a1e5190452be487fcbd45093dca00/src/models/edct.py#L78
+        # Use relative positionalencoding only
+        
+        self.self_positional_encoding = self.self_positional_encoding_k = self.self_positional_encoding_v = None
+        
+        self.self_positional_encoding_k = \
+            RelativePositionalEncoding(self.pe_max_relative_position, self.head_size,
+                                        self.positional_encoding_trainable)
+        self.self_positional_encoding_v = \
+            RelativePositionalEncoding(self.pe_max_relative_position, self.head_size,
+                                        self.positional_encoding_trainable)
 
-        )
+        # transformer blocks 
+        """
+        (경민)
+        positional encoding k,v가 어디서 정의되야 하는걸까?, 왜 overview에는 안그려져 있지?
+        isolate_subnetwork뜻이 뭐지
+        """
+        self.transformer_blocks = nn.ModuleList([self.basic_block_cls(self.seq_hidden_units, 
+                                                                      self.num_heads, self.head_size, 
+                                                                      self.seq_hidden_units * 4,self.dropout_rate,
+                                                                      self.dropout_rate if sub_args.attn_dropout else 0.0,
+                                                                      self_positional_encoding_k=self.self_positional_encoding_k,
+                                                                      self_positional_encoding_v=self.self_positional_encoding_v
+                                                                      ,n_inputs=self.n_inputs,disable_cross_attention = False,
+                                                                      isolate_subnetwork=sub_args.isolate_subnetwork) for _ in range(self.num_layer)])
+
 
         # output layer 
         self.br_head = BROutcomeHead()
@@ -90,10 +142,6 @@ class CT(LightningModule):
         output = self.output_dropout(x)
         br = self.br_head.build_br(output)
         return br
-
-
-            
-
 
 
     def training_step(self, batch, batch_idx):

6d96d74cfa1f6deea88808cce67a343ef8ea9a5d
JayC1208 <gina108@naver.com>
2024-04-13 08:13:32 -0400

    modify tcdf_attention

--- embedding/tcdf_attention.py
+++ embedding/tcdf_attention.py
@@ -19,60 +19,60 @@ from src.data.cancer_sim.dataset import SyntheticCancerDatasetCollection
 
 torch.set_default_dtype(torch.float64)
 
-# cancer_sim 
-num_patients = {'train': 10000, 'val': 10000, 'test': 1000}
-datasetcollection = SyntheticCancerDatasetCollection(chemo_coeff = 3.0, radio_coeff = 3.0, num_patients = num_patients, window_size =15, 
-                                    max_seq_length = 60, projection_horizon = 5, 
-                                    seed = 42, lag = 0, cf_seq_mode = 'sliding_treatment', treatment_mode = 'multiclass')
-datasetcollection.process_data_multi()
+# # cancer_sim 
+# num_patients = {'train': 10000, 'val': 10000, 'test': 1000}
+# datasetcollection = SyntheticCancerDatasetCollection(chemo_coeff = 3.0, radio_coeff = 3.0, num_patients = num_patients, window_size =15, 
+#                                     max_seq_length = 60, projection_horizon = 5, 
+#                                     seed = 42, lag = 0, cf_seq_mode = 'sliding_treatment', treatment_mode = 'multiclass')
+# datasetcollection.process_data_multi()
 
-# Example of iterating over the DataLoader
-config = {
-    "lr" : 0.01,
-    "epochs" : 150,
-    "batch_size": 256,
-    "fc_hidden_units": 32,
-    "has_vital": False
-}
-keys = ['prev_treatments', 'current_treatments', 'current_covariates', 'outputs', 'active_entries', 'unscaled_outputs', 'prev_outputs']
-if config["has_vital"]:
-    keys.append(['vitals', 'next_vitals'])
-for key in keys:
-    observed_nodes_list= list(range(datasetcollection.train_f.data[key].shape[-1]))
-    datasetcollection.train_f.data[key],_,_ = unroll_temporal_data(datasetcollection.train_f.data[key], observed_nodes_list, window_len = 3)
-    dim_X = 0 
-    if key == 'prev_treatments':
-        dim_A = datasetcollection.train_f.data[key].shape[-1] # Dimension of treatments
-    elif key == 'vitals':
-        dim_X = datasetcollection.train_f.data[key].shape[-1] # Dimension of vitals
-    elif key == "outputs":
-        dim_Y = datasetcollection.train_f.data[key].shape[-1] # Dimension of outputs
-    elif key == "current_covariates":
-        dim_V = datasetcollection.train_f.data[key].shape[-1] # Dimension of static inputs
-for key in keys:
-    observed_nodes_list= list(range(datasetcollection.val_f.data[key].shape[-1]))
-    datasetcollection.val_f.data[key],_,_ = unroll_temporal_data(datasetcollection.val_f.data[key], observed_nodes_list, window_len = 3)
-    dim_X = 0 
-    if key == 'prev_treatments':
-        dim_A = datasetcollection.val_f.data[key].shape[-1] # Dimension of treatments
-    elif key == 'vitals':
-        dim_X = datasetcollection.val_f.data[key].shape[-1] # Dimension of vitals
-    elif key == "outputs":
-        dim_Y = datasetcollection.val_f.data[key].shape[-1] # Dimension of outputs
-    elif key == "current_covariates":
-        dim_V = datasetcollection.val_f.data[key].shape[-1] # Dimension of static inputs
+# # Example of iterating over the DataLoader
+# config = {
+#     "lr" : 0.01,
+#     "epochs" : 150,
+#     "batch_size": 256,
+#     "fc_hidden_units": 32,
+#     "has_vital": False
+# }
+# keys = ['prev_treatments', 'current_treatments', 'current_covariates', 'outputs', 'active_entries', 'unscaled_outputs', 'prev_outputs']
+# if config["has_vital"]:
+#     keys.append(['vitals', 'next_vitals'])
+# for key in keys:
+#     observed_nodes_list= list(range(datasetcollection.train_f.data[key].shape[-1]))
+#     datasetcollection.train_f.data[key],_,_ = unroll_temporal_data(datasetcollection.train_f.data[key], observed_nodes_list, window_len = 3)
+#     dim_X = 0 
+#     if key == 'prev_treatments':
+#         dim_A = datasetcollection.train_f.data[key].shape[-1] # Dimension of treatments
+#     elif key == 'vitals':
+#         dim_X = datasetcollection.train_f.data[key].shape[-1] # Dimension of vitals
+#     elif key == "outputs":
+#         dim_Y = datasetcollection.train_f.data[key].shape[-1] # Dimension of outputs
+#     elif key == "current_covariates":
+#         dim_V = datasetcollection.train_f.data[key].shape[-1] # Dimension of static inputs
+# for key in keys:
+#     observed_nodes_list= list(range(datasetcollection.val_f.data[key].shape[-1]))
+#     datasetcollection.val_f.data[key],_,_ = unroll_temporal_data(datasetcollection.val_f.data[key], observed_nodes_list, window_len = 3)
+#     dim_X = 0 
+#     if key == 'prev_treatments':
+#         dim_A = datasetcollection.val_f.data[key].shape[-1] # Dimension of treatments
+#     elif key == 'vitals':
+#         dim_X = datasetcollection.val_f.data[key].shape[-1] # Dimension of vitals
+#     elif key == "outputs":
+#         dim_Y = datasetcollection.val_f.data[key].shape[-1] # Dimension of outputs
+#     elif key == "current_covariates":
+#         dim_V = datasetcollection.val_f.data[key].shape[-1] # Dimension of static inputs
     
     
-# dim_A = 4  # Dimension of treatments
-# dim_X = 0  # Dimension of vitals
-# dim_Y = 1  # Dimension of outputs
-# dim_V = 1  # Dimension of static inputs
+# # dim_A = 4  # Dimension of treatments
+# # dim_X = 0  # Dimension of vitals
+# # dim_Y = 1  # Dimension of outputs
+# # dim_V = 1  # Dimension of static inputs
 
-batch_size = config['batch_size']
-epoch = config['epochs']
-fc_hidden_units = config['fc_hidden_units']
-train_loader = DataLoader(datasetcollection.train_f, batch_size=batch_size, shuffle=True)
-val_loader = DataLoader(datasetcollection.val_f, batch_size=batch_size, shuffle=False)
+# batch_size = config['batch_size']
+# epoch = config['epochs']
+# fc_hidden_units = config['fc_hidden_units']
+# train_loader = DataLoader(datasetcollection.train_f, batch_size=batch_size, shuffle=True)
+# val_loader = DataLoader(datasetcollection.val_f, batch_size=batch_size, shuffle=False)
 
 
 checkpoint_path = "../real_weight/cancersim_unroll_3_3_1.pt"
@@ -80,6 +80,7 @@ embedding_space = EmbeddingSpace(checkpoint_path)
 variable = "treatment"
 results = embedding_space.make_heads(variable = variable)
 i1, i2 = np.random.randint(embedding_space.num_layers), np.random.randint(embedding_space.num_heads)
+print(i1, i2)
 W_Q_tmp, W_K_tmp = results[3][i1, i2, :], results[4][i1, i2, :]
 attn_score = (embedding_space.emb_inv_A @ (W_Q_tmp @ W_K_tmp.T) @ embedding_space.emb_inv_A.T)
 # attn_score += abs(attn_score.min())  # make it above zero
@@ -156,68 +157,68 @@ print("potential causes: ", potential_idx)
 scramble_idx = list(set(potential_idx[:,1]))
 print("scramble:", scramble_idx)
 
-validated = copy.deepcopy(scramble_idx)
+# validated = copy.deepcopy(scramble_idx)
 
-#Apply PIVM (permutes the values) to check if potential cause is true cause
-for idx in scramble_idx:
-    random.seed(seed)
+# #Apply PIVM (permutes the values) to check if potential cause is true cause
+# for idx in scramble_idx:
+#     random.seed(seed)
 
-    firstloss = 10.0  # temporary value
+#     firstloss = 10.0  # temporary value
     
-    realloss = 0.0
-    testloss = 0.0
-    for batch_idx, batch in enumerate(train_loader):
+#     realloss = 0.0
+#     testloss = 0.0
+#     for batch_idx, batch in enumerate(train_loader):
         
-        # prev_outputs = batch["prev_outputs"]
-        # print(prev_outputs.shape)    # [batch_size, 57, 3]
-        # static_features = batch["static_features"]
-        # print(static_features.shape)   # [batch_size, 1]
-        # active_entries = batch['active_entries']
-        # print(active_entries.shape)  # [batch_size, 57, 3]
-        # output = batch['outputs'] # [batch_size, 57, 3]
-        # print(output.shape)
+#         # prev_outputs = batch["prev_outputs"]
+#         # print(prev_outputs.shape)    # [batch_size, 57, 3]
+#         # static_features = batch["static_features"]
+#         # print(static_features.shape)   # [batch_size, 1]
+#         # active_entries = batch['active_entries']
+#         # print(active_entries.shape)  # [batch_size, 57, 3]
+#         # output = batch['outputs'] # [batch_size, 57, 3]
+#         # print(output.shape)
 
-        # firstloss is loss when model is trained for one epoch
-        # realloss is loss when model is fully trained
-        realloss += embedding_space.model.training_step(batch, 1, trainer=False).cpu().data.item()   # give 1 to batch_idx cause is not used in the function (no need)
-        # print(realloss)
+#         # firstloss is loss when model is trained for one epoch
+#         # realloss is loss when model is fully trained
+#         realloss += embedding_space.model.training_step(batch, 1, trainer=False).cpu().data.item()   # give 1 to batch_idx cause is not used in the function (no need)
+#         # print(realloss)
 
-        if variable == "treatment": 
-            x_test2 = batch["prev_treatments"].clone().cpu().numpy()  # [batch_size, 57, 12]
-            perm = np.arange(x_test2[:,:,idx].shape[1])
-            np.random.shuffle(perm)
-            x_test2[:,:,idx] = x_test2[:,:,idx][:,perm]
-            shuffled_prev_treat = torch.from_numpy(x_test2)
+#         if variable == "treatment": 
+#             x_test2 = batch["prev_treatments"].clone().cpu().numpy()  # [batch_size, 57, 12]
+#             perm = np.arange(x_test2[:,:,idx].shape[1])
+#             np.random.shuffle(perm)
+#             x_test2[:,:,idx] = x_test2[:,:,idx][:,perm]
+#             shuffled_prev_treat = torch.from_numpy(x_test2)
 
-            x_test3 = batch["current_treatments"].clone().cpu().numpy()  # [batch_size, 57, 12]
-            perm = np.arange(x_test3[:,:,idx].shape[1])
-            np.random.shuffle(perm)
-            x_test3[:,:,idx] = x_test3[:,:,idx][:,perm]
-            shuffled_cur_treat = torch.from_numpy(x_test3)
+#             x_test3 = batch["current_treatments"].clone().cpu().numpy()  # [batch_size, 57, 12]
+#             perm = np.arange(x_test3[:,:,idx].shape[1])
+#             np.random.shuffle(perm)
+#             x_test3[:,:,idx] = x_test3[:,:,idx][:,perm]
+#             shuffled_cur_treat = torch.from_numpy(x_test3)
 
-            batch['prev_treatments'] = shuffled_prev_treat
-            batch['current_treatments'] = shuffled_cur_treat
+#             batch['prev_treatments'] = shuffled_prev_treat
+#             batch['current_treatments'] = shuffled_cur_treat
 
-        testloss += embedding_space.model.training_step(batch, 1, trainer=False).cpu().data.item()   # give 1 to batch_idx cause is not used in the function (no need)
-        # print(testloss)
+#         testloss += embedding_space.model.training_step(batch, 1, trainer=False).cpu().data.item()   # give 1 to batch_idx cause is not used in the function (no need)
+#         # print(testloss)
         
-        # # permute by batch columns
-        # X_test2 = X_train.clone().cpu().numpy()
-        # random.shuffle(X_test2[:,idx,:][0])
-        # shuffled = torch.from_numpy(X_test2)
-        # if cuda:
-        #     shuffled=shuffled.cuda()
-        # model.eval()
-        # output = model(shuffled)
-        # testloss = F.mse_loss(output, Y_train)
-        # testloss = testloss.cpu().data.item()
+#         # # permute by batch columns
+#         # X_test2 = X_train.clone().cpu().numpy()
+#         # random.shuffle(X_test2[:,idx,:][0])
+#         # shuffled = torch.from_numpy(X_test2)
+#         # if cuda:
+#         #     shuffled=shuffled.cuda()
+#         # model.eval()
+#         # output = model(shuffled)
+#         # testloss = F.mse_loss(output, Y_train)
+#         # testloss = testloss.cpu().data.item()
         
-    diff = firstloss-realloss
-    testdiff = firstloss-testloss
-    print("diff:", diff)
-    print("testdiff:", testdiff)
-    significance = 0.8  # use value from TCDF
-    if testdiff>(diff*significance): 
-        print("removed!")
-        validated.remove(idx) 
+#     diff = firstloss-realloss
+#     testdiff = firstloss-testloss
+#     print("diff:", diff)
+#     print("testdiff:", testdiff)
+#     significance = 0.8  # use value from TCDF
+#     if testdiff>(diff*significance): 
+#         print("removed!")
+#         validated.remove(idx) 
 
